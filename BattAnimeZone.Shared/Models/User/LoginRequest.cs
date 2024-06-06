434f37aba1f428faaef259b891cd5ea60d001331
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-07 00:20:50 +0200

    Basic JWT Authentication done. Needs registering and further work.

---
+++ BattAnimeZone.Shared/Models/User/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BattAnimeZone.Shared.Models.User
+{
+	public class LoginRequest
+	{
+		public string UserName { get; set; }
+		public string Password { get; set; }
+	}
+}
---
+++ BattAnimeZone/BattAnimeZone/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using BattAnimeZone.Authentication;
+using BattAnimeZone.Services;
+using BattAnimeZone.Shared.Models.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BattAnimeZone.Controllers
+{
+	[Route("api/AccountController")]
+	[ApiController]
+	public class AccountController : ControllerBase
+	{
+		private UserAccountService _userAccountService;
+
+		public AccountController(UserAccountService userAccountService)
+		{
+			_userAccountService = userAccountService;
+		}
+
+		[HttpPost]
+		[Route("Login")]
+		[AllowAnonymous]
+		public ActionResult<UserSession> Login([FromBody] LoginRequest loginRequest)
+		{
+            Console.WriteLine("belépett ide");
+            var jwtAuthenticationManager = new JwtAuthenticationManager(_userAccountService);
+			var userSession = jwtAuthenticationManager.GenerateJwtToken(loginRequest.UserName, loginRequest.Password);
+			if (userSession is null)
+				return Unauthorized();
+			else
+				return userSession;
+		}
+	}
+}
