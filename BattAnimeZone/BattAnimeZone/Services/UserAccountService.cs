8bb0dcb66da8614580fd71e599604999bc5e6882
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-07 16:48:52 +0200

    Deleted CW

--- BattAnimeZone/BattAnimeZone/Services/UserAccountService.cs
+++ BattAnimeZone/BattAnimeZone/Services/UserAccountService.cs
@@ -19,9 +19,6 @@ namespace BattAnimeZone.Services
 
 		public UserAccount? GetUserAccountByUserName(string userName)
 		{
-			foreach (var userAccount in _userAccountList) {
-                Console.WriteLine(userAccount.UserName);
-            }
 			return _userAccountList.FirstOrDefault(x => x.UserName == userName);
 		}
 
