1f61ed05e9a2ed5480b3e681f976b72be024e4ed
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-10 20:34:40 +0200

    Renamed Anime attributes

--- BattAnimeZone.Shared/Models/Anime/Anime.cs
+++ BattAnimeZone.Shared/Models/Anime/Anime.cs
@@ -3,19 +3,19 @@
 	public class Anime
 	{
 		public int Mal_id { get; set; } = -1;
-		//public string Url { get; set; } = string.Empty;
+		public string Url { get; set; } = string.Empty;
 		public string Title { get; set; } = string.Empty;
-		public string Title_english { get; set; } = string.Empty;
-		public string Title_japanese { get; set; } = string.Empty;
-		//public List<string> Ttile_synonyms { get; set; } = new List<string> {""};
-		public string Media_type { get; set; } = string.Empty;
+		public string TitleEnglish { get; set; } = string.Empty;
+		public string TitleJapanese { get; set; } = string.Empty;
+		public List<string> TtileSynonyms { get; set; } = new List<string> {""};
+		public string MediaType  { get; set; } = string.Empty;
 		public string Source { get; set; } = string.Empty;
 		public int Episodes { get; set; } = -1;
 		public string Status { get; set; } = string.Empty;
 		public string Duration { get; set; } = string.Empty;
 		public string Rating { get; set; } = string.Empty;
 		public float Score { get; set; } = -1;
-		public int Scored_by { get; set; } = -1;
+		public int ScoredBy { get; set; } = -1;
 		public int Rank {  get; set; } = -1;
 		public int Popularity {  get; set; } = -1;
 		public int Members {  get; set; } = -1;
@@ -30,28 +30,32 @@
 		public List<Entry> Genres { get; set; }
 		public List<Entry> Themes { get; set; }
 		public List<Relations> Relations { get; set; }
-		//public List<External> Externals { get; set; }
-		//public List<Streaming> Streamings { get; set; }
-		//public string Image_jpg_url { get; set; } = string.Empty;
-		//public string Image_small_jpg_url { get; set; } = string.Empty;
-		//public string Image_large_jpg_url { get; set; } = string.Empty;
-		//public string Image_webp_url { get; set; } = string.Empty;
-		//public string Image_small_webp_url { get; set; } = string.Empty;
-		public string Image_large_webp_url { get; set; } = string.Empty;
-		//public string Trailer_url { get; set; } = string.Empty;
-		//public string Trailer_embed_url { get; set; } = string.Empty;
-		//public string Trailer_image_url { get; set; } = string.Empty;
-		//public string Trailer_image_small_url { get; set; } = string.Empty;
-		//public string Trailer_image_medium_url { get; set; } = string.Empty;
-		//public string Trailer_image_large_url { get; set; } = string.Empty;
-		//public string Trailer_image_maximum_url { get; set; } = string.Empty;
-		public int Aired_from_day { get; set; } = -1;
-		public int Aired_from_month { get; set; } = -1;
-		public int Aired_from_year { get; set; } = -1;
-		public int Aired_to_day { get; set; } = -1;
-		public int Aired_to_month { get; set; } = -1;
-		public int Aired_to_year { get; set; } = -1;
-		public string Aired_string { get; set; } = string.Empty;
+
+		public List<External> Externals { get; set; }
+		public List<Streaming> Streamings { get; set; }
+		public string ImageJpgUrl { get; set; } = string.Empty;
+		public string ImageSmallJpgUrl { get; set; } = string.Empty;
+		public string ImageLargeJpgUrl { get; set; } = string.Empty;
+		public string ImageWebpUrl { get; set; } = string.Empty;
+		public string ImageSmallWebpUrl { get; set; } = string.Empty;
+
+		public string ImageLargeWebpUrl { get; set; } = string.Empty;
+
+		public string TrailerUrl { get; set; } = string.Empty;
+		public string TrailerEmbedUrl { get; set; } = string.Empty;
+		public string TrailerImageUrl { get; set; } = string.Empty;
+		public string TrailerImageSmallUrl { get; set; } = string.Empty;
+		public string TrailerImageMediumUrl { get; set; } = string.Empty;
+		public string TrailerImageLargeUrl { get; set; } = string.Empty;
+		public string TrailerImageMaximumUrl { get; set; } = string.Empty;
+
+		public int AiredFromDay { get; set; } = -1;
+		public int AiredFromMonth { get; set; } = -1;
+		public int AiredFromYear { get; set; } = -1;
+		public int AiredToDay { get; set; } = -1;
+		public int AiredToMonth { get; set; } = -1;
+		public int AiredToYear { get; set; } = -1;
+		public string AiredString { get; set; } = string.Empty;
 
 
 
