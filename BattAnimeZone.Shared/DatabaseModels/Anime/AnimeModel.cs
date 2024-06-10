88e84e497c31434037b9db3ea8e0390ffbecbd9e
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-11 00:16:53 +0200

    Half of the anime database fill functions. Need to redo the controller service logic later.

---
+++ BattAnimeZone.Shared/DatabaseModels/Anime/AnimeModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattAnimeZone.Shared.DatabaseModels.Anime
+{
+    [Table("Anime")]
+    public class AnimeModel
+    {
+        [Key]
+        [Column("id")]
+        public int Mal_id { get; set; }
+
+        [Column("title")]
+        public string Title { get; set; } = "";
+
+        [Column("title_english")]
+        public string TitleEnglish { get; set; } = "";
+
+        [Column("title_japanese")]
+        public string TitleJapanese { get; set; } = "";
+
+        [Column("title_synonyms")]
+        public string TitleSynonyms { get; set; } = "";
+
+        [Column("media_type")]
+        public string MediaType { get; set; } = "";
+
+        [Column("source")]
+        public string Source { get; set; } = "";
+
+        [Column("episodes")]
+        public int Episodes { get; set; } = 0;
+
+        [Column("duration")]
+        public string Duration { get; set; } = "";
+
+        [Column("rating")]
+        public string Rating { get; set; } = "";
+
+        [Column("score")]
+        public float Score { get; set; } = -1;
+
+        [Column("scored_by")]
+        public int ScoredBy { get; set; } = -1;
+
+        [Column("rank")]
+        public int Rank { get; set; } = -1;
+
+        [Column("popularity")]
+        public int Popularity { get; set; } = -1;
+
+        [Column("members")]
+        public int Members { get; set; } = -1;
+
+        [Column("favorites")]
+        public int Favorites { get; set; } = -1;
+
+        [Column("synopsis")]
+        public string Synopsis { get; set; } = "";
+
+        [Column("background")]
+        public string Background { get; set; } = "";
+
+        [Column("season")]
+        public string Season { get; set; } = "";
+
+        [Column("year")]
+        public int Year { get; set; } = -1;
+
+        [Column("image_jpg_url")]
+        public string ImageJpgUrl { get; set; } = "";
+
+        [Column("image_small_jpg_url")]
+        public string ImageSmallJpgUrl { get; set; } = "";
+
+        [Column("image_large_jpg_url")]
+        public string ImageLargeJpgUrl { get; set; } = "";
+
+        [Column("image_webp_url")]
+        public string ImageWebpUrl { get; set; } = "";
+
+        [Column("image_small_webp_url")]
+        public string ImageSmallWebpUrl { get; set; } = "";
+
+        [Column("image_large_webp_url")]
+        public string ImageLargeWebpUrl { get; set; } = "";
+
+        [Column("trailer_url")]
+        public string TrailerUrl { get; set; } = "";
+
+        [Column("trailer_embed_url")]
+        public string TrailerEmbedUrl { get; set; } = "";
+
+        [Column("trailer_image_url")]
+        public string TrailerImageUrl { get; set; } = "";
+
+        [Column("trailer_image_small_url")]
+        public string TrailerImageSmallUrl { get; set; } = "";
+
+        [Column("trailer_image_medium_url")]
+        public string TrailerImageMediumUrl { get; set; } = "";
+
+        [Column("trailer_image_large_url")]
+        public string TrailerImageLargeUrl { get; set; } = "";
+
+        [Column("trailer_image_maximum_url")]
+        public string TrailerImageMaximumUrl { get; set; } = "";
+
+        [Column("aired_from_day")]
+        public int AiredFromDay { get; set; } = -1;
+
+        [Column("aired_from_month")]
+        public int AiredFromMonth { get; set; } = -1;
+
+        [Column("aired_from_year")]
+        public int AiredFromYear { get; set; } = -1;
+
+        [Column("aired_to_day")]
+        public int AiredToDay { get; set; } = -1;
+
+        [Column("aired_to_month")]
+        public int AiredToMonth { get; set; } = -1;
+
+        [Column("aired_to_year")]
+        public int AiredToYear { get; set; } = -1;
+
+        [Column("aired_string")]
+        public string AiredString { get; set; } = "";
+    }
+}
---
+++ BattAnimeZone/BattAnimeZone/Services/AnimeService._database.cs
@@ -0,0 +1,16 @@
+﻿namespace BattAnimeZone.Services
+{
+	public partial class AnimeService
+	{
+		private bool databaseFilled = false;
+		public bool isDatabaseFilled()
+		{
+			return this.databaseFilled;
+		}
+
+		public void changeFilledFlag()
+		{
+			this.databaseFilled = true;
+		}
+	}
+}
