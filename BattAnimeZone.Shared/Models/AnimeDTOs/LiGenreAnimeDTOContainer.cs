736148cf652c24d4884bd481493c9c3581a7fb6c
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-13 20:23:47 +0200

    ListGenreAnime on DB.

---
+++ BattAnimeZone.Shared/Models/AnimeDTOs/LiGenreAnimeDTOContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattAnimeZone.Shared.Models.AnimeDTOs
+{
+    public class LiGenreAnimeDTOContainer
+    {
+        public string GenreName { get; set; }
+        public List<LiGenreAnimeDTO> Animes { get; set; }
+    }
+}
