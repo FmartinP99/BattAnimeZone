2ec7de845434143c2aaf16d1ded44511f47d2585
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-15 20:15:55 +0200

    Register form validation.

---
+++ BattAnimeZone.Shared/Models/User/Validation/PasswordValidator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BattAnimeZone.Shared.Models.User.Validation
+{
+    public class PasswordValidator : ValidationAttribute
+    {
+       
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is string password)
+            {
+                string errorMessage = "Password ";
+                if (password.Length < 10 || password.Length > 30)
+                {
+                    errorMessage += "must be between 10 and 30 characters, ";
+                }
+                if (!Regex.IsMatch(password, @"[0-9]"))
+                {
+                    errorMessage += "must contain at least one number, ";
+                }
+                if (!Regex.IsMatch(password, @"[a-z]"))
+                {
+                    errorMessage += "must contain at least one lowercase letter, ";
+                }
+                if (!Regex.IsMatch(password, @"[A-Z]"))
+                {
+                    errorMessage += "must contain at least one uppercase letter, ";
+                }
+                if (errorMessage != "Password ")
+                {
+                    if (errorMessage.EndsWith(", "))
+                    {
+                        errorMessage = errorMessage.Substring(0, errorMessage.Length - 2) + ".";
+                    }
+                    int lastCommaIndex = errorMessage.LastIndexOf(",");
+                    if (lastCommaIndex != -1)
+                    {
+                        errorMessage = errorMessage.Remove(lastCommaIndex, 1).Insert(lastCommaIndex, " and");
+                    }
+                    return new ValidationResult(errorMessage);
+                }
+                return ValidationResult.Success;
+            }
+            return new ValidationResult("Invalid password format.");
+        }
+
+    }
+}
---
+++ BattAnimeZone.Shared/Models/User/Validation/UsernameValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BattAnimeZone.Shared.Models.User.Validation
+{
+    public class UsernameValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is string username)
+            {
+                if (username.Length < 6 || username.Length > 30)
+                {
+                    return new ValidationResult("Username must be between 6 and 30 characters.");
+                }
+                return ValidationResult.Success;
+            }
+            return new ValidationResult("Invalid username format.");
+        }
+    }
+}
