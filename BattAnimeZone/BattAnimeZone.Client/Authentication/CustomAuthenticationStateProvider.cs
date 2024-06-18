842e0d83f6fc9d7d245a077a6bb1252adefea7c7
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-18 18:45:58 +0200

    deleted cw

--- BattAnimeZone/BattAnimeZone.Client/Authentication/CustomAuthenticationStateProvider.cs
+++ BattAnimeZone/BattAnimeZone.Client/Authentication/CustomAuthenticationStateProvider.cs
@@ -46,10 +46,7 @@ namespace BattAnimeZone.Client.Authentication
             {
                 var userSession = await _localStorage.GetItemAsync<UserSession>("UserSession");
                 if (userSession == null)
-                    return await Task.FromResult(new AuthenticationState(_anonymous));
-
-                await Console.Out.WriteLineAsync($"usersession expirytimesttamp {userSession.ExpiryTimeStamp}");
-                await Console.Out.WriteLineAsync($"nowtime  {DateTime.Now.ToUniversalTime()}");
+                    return await Task.FromResult(new AuthenticationState(_anonymous))
 
                 if (DateTime.Now.ToUniversalTime() > userSession.ExpiryTimeStamp)
                 {
