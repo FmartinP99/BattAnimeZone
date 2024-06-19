e8e8ce2ea7251e3711cf8d7ebc58b4d8b2418942
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-19 11:24:16 +0200

    Added localstorage encryption

---
+++ BattAnimeZone/BattAnimeZone.Client/Extensions/LocalStorageServiceExtension.cs
@@ -0,0 +1,26 @@
+﻿using Blazored.LocalStorage;
+using System.Text;
+using System.Text.Json;
+
+namespace BattAnimeZone.Client.Extensions
+{
+    public static class LocalStorageServiceExtension
+    {
+        public static async Task SaveItemEncryptedAsync<T>(this ILocalStorageService localStorageService, string key, T item)
+        {
+            var itemJson = JsonSerializer.Serialize(item);
+            var itemJsonBytes = Encoding.UTF8.GetBytes(itemJson);
+            var base64Json = Convert.ToBase64String(itemJsonBytes);
+            await localStorageService.SetItemAsync(key, base64Json);
+        }
+
+        public static async Task<T> ReadEncryptedItemAsync<T>(this ILocalStorageService localStorageService, string key)
+        {
+            var base64Json = await localStorageService.GetItemAsync<string>(key);
+            var itemJsonBytes = Convert.FromBase64String(base64Json);
+            var itemJson = Encoding.UTF8.GetString(itemJsonBytes);
+            var item = JsonSerializer.Deserialize<T>(itemJson);
+            return item;
+        }
+    }
+}
