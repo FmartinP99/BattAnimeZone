97d603eba6e1ed90d56873277dbe4c555a3d7570
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-20 11:50:52 +0200

    timer hack to check if the authentication state changed, and if it did it'd force-refresh the current page to update the UI

--- BattAnimeZone/BattAnimeZone.Client/Authentication/AuthenticationMessageHandler.cs
+++ BattAnimeZone/BattAnimeZone.Client/Authentication/AuthenticationMessageHandler.cs
@@ -1,19 +1,23 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
-using System.Security.Claims;
+﻿
+
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BattAnimeZone.Client.Authentication
 {
     public class AuthenticationMessageHandler : DelegatingHandler
     {
-        private readonly CustomAuthenticationStateProvider _authenticationStateProvider;
-        public AuthenticationMessageHandler(CustomAuthenticationStateProvider authenticationStateProvider)
+        private readonly AuthenticationStateProvider _authStateProvider;
+        private readonly CustomAuthenticationStateProvider _customAuthenticationStateProvider;
+        public AuthenticationMessageHandler(AuthenticationStateProvider authStateProvider)
         {
-            _authenticationStateProvider = authenticationStateProvider;
+            _authStateProvider = authStateProvider;
+            _customAuthenticationStateProvider = (CustomAuthenticationStateProvider)_authStateProvider;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            await _authenticationStateProvider.AuthenticationStateChecker();
+            //DO SOMETHING BEFORE EVERY HTTPRQUEST SENT
+            //await _customAuthenticationStateProvider.AuthenticationStateChecker();
             return await base.SendAsync(request, cancellationToken);
         }
 
