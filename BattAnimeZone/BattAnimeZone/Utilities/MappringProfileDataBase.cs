42c3a46ecaceb01f553c78ee5a5f096cb2c4727b
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-21 15:14:57 +0200

    Auto truncating HomePage synopsis after 500 character to reduce payload

--- BattAnimeZone/BattAnimeZone/Utilities/MappringProfileDataBase.cs
+++ BattAnimeZone/BattAnimeZone/Utilities/MappringProfileDataBase.cs
@@ -10,7 +10,44 @@ namespace BattAnimeZone.Utilities
 	{
 		public MappringProfileDataBase()
 		{
-			CreateMap<AnimeModel, AnimeHomePageDTO>();
+			CreateMap<AnimeModel, AnimeHomePageDTO>()
+			.ForMember(dest => dest.Synopsis, opt => opt.MapFrom<SynopsisTruncateResolver>());
+		}
+	}
+
+
+
+
+	public class SynopsisTruncateResolver : IValueResolver<AnimeModel, AnimeHomePageDTO, string>
+	{
+		public string Resolve(AnimeModel source, AnimeHomePageDTO destination, string destMember, ResolutionContext context)
+		{
+
+			if (string.IsNullOrEmpty(source.Synopsis))
+			{
+				return string.Empty;
+			}
+
+
+			if (source.Synopsis.Length <= 500)
+			{
+				return source.Synopsis;
+			}
+			else
+			{
+
+				int lastIndex = source.Synopsis.LastIndexOf(' ', 500);
+
+
+				if (lastIndex <= 0)
+				{
+					return source.Synopsis.Substring(0, 500) + "...[Click for more!]";
+				}
+				else
+				{
+					return source.Synopsis.Substring(0, lastIndex) + "...[Click for more!]";
+				}
+			}
 		}
 	}
 }
