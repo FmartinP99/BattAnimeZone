28adbb899464f36a68c58b709ba90e8d922d3010
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-22 14:11:47 +0200

    Updated filtering functionality and UI for the ListAnime page.

--- BattAnimeZone/BattAnimeZone/Services/DataBaseService.cs
+++ BattAnimeZone/BattAnimeZone/Services/DataBaseService.cs
@@ -74,7 +74,7 @@ namespace BattAnimeZone.Services
 		{
 			using (var _context = await _dbContextFactory.CreateDbContextAsync())
 			{
-				List<int> distinctYears = await _context.DistinctYears.Select(x => x.Year).ToListAsync();
+				List<int> distinctYears = await _context.DistinctYears.Select(x => x.Year).OrderBy(x => x).ToListAsync();
 				return distinctYears;
 			}
 		}
