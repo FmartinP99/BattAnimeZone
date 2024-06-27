423cd0d4f707fe2c54c63b7fb1b1da585bd57a7e
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-27 21:32:30 +0200

    Supabase support for listgenreanimes

---
+++ BattAnimeZone/BattAnimeZone/Services/SupaBaseService.ListGenreAnime.cs
@@ -0,0 +1,32 @@
+﻿using BattAnimeZone.Shared.Models.AnimeDTOs;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Text.Json;
+
+namespace BattAnimeZone.Services
+{
+    public partial class SupaBaseService
+    {
+        public async Task<LiGenreAnimeDTOContainer?> GetAnimesForListGenreAnimes(int genre_id)
+        {
+
+            var rpcParameters = new
+            {
+                _genre_id = genre_id,
+            };
+
+            var response = await _client.Rpc("get_genre_animes", rpcParameters);
+
+
+            if (response.ResponseMessage.IsSuccessStatusCode)
+            {
+                var return_list = JsonSerializer.Deserialize<List<LiGenreAnimeDTOContainer>>(response.Content, jsonOptions).FirstOrDefault();
+                return return_list;
+            }
+
+            return null;
+
+
+
+        }
+    }
+}
