8f6ea3a0c5053a699a83f006cfca9b5b5f2d38c5
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-27 17:20:15 +0200

    GenreList now supports supabase

---
+++ BattAnimeZone/BattAnimeZone/Services/Interfaces/IDataBaseService.cs
@@ -0,0 +1,11 @@
+﻿using BattAnimeZone.Shared.Models.GenreDTOs;
+
+namespace BattAnimeZone.Services.Interfaces
+{
+	public interface IDataBaseService
+	{
+		public Task<List<AnimeGenreDTO>?> GetGenres();
+		public Task<Dictionary<int, int>> GetAnimesPerGenreIdCount();
+
+	}
+}
