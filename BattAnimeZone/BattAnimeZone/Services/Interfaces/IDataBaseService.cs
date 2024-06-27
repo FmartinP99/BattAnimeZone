5685a36307735f99f59093c63cc207a965b92400
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-06-27 20:18:48 +0200

    Added supabase support for listanime page

--- BattAnimeZone/BattAnimeZone/Services/Interfaces/IDataBaseService.cs
+++ BattAnimeZone/BattAnimeZone/Services/Interfaces/IDataBaseService.cs
@@ -5,11 +5,11 @@ namespace BattAnimeZone.Services.Interfaces
 {
 	public interface IDataBaseService
 	{
-		public Task<List<string?>> GetDistinctMediaTypes();
-		public Task<List<int?>> GetDistinctYears();
+		public Task<List<string?>?> GetDistinctMediaTypes();
+		public Task<List<int?>?> GetDistinctYears();
 		public Task<List<AnimeGenreDTO>?> GetGenres();
-		public Task<Dictionary<int, int>> GetAnimesPerGenreIdCount();
+		public Task<Dictionary<int, int>?> GetAnimesPerGenreIdCount();
 		public Task<AnimeRelationsKeyDTO?> GetRelations(int mal_id);
-
-	}
+		public Task<List<LiAnimeDTO>?> GetFilteredAnimes(List<int>? genres, List<string>? mediaTypes, int? yearlower, int? yearupper);
+    }
 }
---
+++ BattAnimeZone/BattAnimeZone/Services/SupaBaseService.ListAnime.cs
@@ -0,0 +1,34 @@
+﻿using BattAnimeZone.Shared.Models.AnimeDTOs;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Text.Json;
+
+namespace BattAnimeZone.Services
+{
+	public partial class SupaBaseService
+	{
+
+		public async Task<List<LiAnimeDTO>?> GetFilteredAnimes(List<int>? genres, List<string>? mediaTypes, int? yearlower, int? yearupper)
+		{
+			var rpcParameters = new
+			{
+				genres = genres.Count > 0 ? genres : null,
+				media_types = mediaTypes.Count > 0 ? mediaTypes : null,
+				year_lower = yearlower,
+				year_upper = yearupper
+			};
+
+			var response = await _client.Rpc("get_filtered_animes", rpcParameters);
+
+            if (response.ResponseMessage.IsSuccessStatusCode)
+			{
+                
+                var return_list = JsonSerializer.Deserialize<List<LiAnimeDTO>>(response.Content, jsonOptions);
+				return return_list;
+			}
+
+			return null;
+
+		}
+
+	}
+}
